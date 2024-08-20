33448db5ddd871a0a3c7186435344a42b6ecb768
Perdana Main <firman.fp123@gmail.com>
2024-08-21 01:03:55 +0700

    add user dashboard

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -5,10 +5,6 @@ const Home = () => {
     <div className="p-5 mb-4 bg-light rounded-3 shadow-sm">
       <div className="container-fluid py-5">
         <h1 className="display-5 fw-bold">FULLTSACK JAVASCRIPT DEVELOPER</h1>
-        <p className="col-md-12 fs-4">
-          Belajar Full Stack JavaScript Developer dengan Express dan React di
-          SantriKoding.com
-        </p>
         <hr />
         <Link to="/register" className="btn btn-primary btn-lg me-3">
           REGISTER
